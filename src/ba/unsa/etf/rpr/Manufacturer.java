db32d5371e6208df431bc3b3bb55d3ca6322d67a
nhastor1 <nhastor1@etf.unsa.ba>
2020-02-10 14:55:02 +0100

    Manufacturer class is made

--- src/ba/unsa/etf/rpr/Manufacturer.java
+++ src/ba/unsa/etf/rpr/Manufacturer.java
@@ -1,5 +1,32 @@
 package ba.unsa.etf.rpr;
 
-public class Manufacturer {
+import ba.unsa.etf.rpr.Person.LegalPerson;
 
+public class Manufacturer {
+    int id;
+    LegalPerson manufacturer;
+
+    public Manufacturer() {
+    }
+
+    public Manufacturer(int id, LegalPerson manufacturer) {
+        this.id = id;
+        this.manufacturer = manufacturer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LegalPerson getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(LegalPerson manufacturer) {
+        this.manufacturer = manufacturer;
+    }
 }
