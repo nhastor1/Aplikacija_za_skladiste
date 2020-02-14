af140d1ff3758421279584d72ae0ef239d4fe3fe
nhastor1 <nhastor1@etf.unsa.ba>
2020-02-14 13:21:29 +0100

    NaturalPersonDAO

--- src/ba/unsa/etf/rpr/DAO/ManufacturerDAO.java
+++ src/ba/unsa/etf/rpr/DAO/ManufacturerDAO.java
@@ -1,6 +1,6 @@
 package ba.unsa.etf.rpr.DAO;
 
-import ba.unsa.etf.rpr.Location.Location;
+import ba.unsa.etf.rpr.Manufacturer;
 import ba.unsa.etf.rpr.Manufacturer;
 import ba.unsa.etf.rpr.User.Administrator;
 import ba.unsa.etf.rpr.User.User;
@@ -8,46 +8,104 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class ManufacturerDAO {
     private static ManufacturerDAO instance = null;
     private Connection conn;
-    private ObservableList<User> listManufacturers = FXCollections.observableArrayList();
-    private PreparedStatement allManufacturerQuery, getManufacturerQueryFromID, addManufacturerQuery, removeManufacturerQuery, getManufacturerIDQuery;
+    private ObservableList<Manufacturer> listManufacturers = FXCollections.observableArrayList();
+    private PreparedStatement allManufacturerQuery, getManufacturerQueryFromID, addManufacturerQuery, getManufacturerIDQuery;
     private int freeID;
 
     private ManufacturerDAO() {
         conn = MainDAO.getInstance().getConn();
         try {
             allManufacturerQuery = conn.prepareStatement("SELECT * FROM Manufacturer");
-            addManufacturerQuery = conn.prepareStatement("INSERT INTO Manufacturer VALUES(?,?,?,?,?,?,?)");
-//            updateUserQuery = conn.prepareStatement("UPDATE Manufacturer SET first_name=?, last_name=?, email=?, username=?, password=?, admin=? WHERE id=?");
-//
-            removeManufacturerQuery = conn.prepareStatement("DELETE FROM Manufacturer WHERE id=?");
-//            countUsernameQuery = conn.prepareStatement("SELECT Count(*) FROM Manufacturer WHERE username=? AND id<>?");
             getManufacturerQueryFromID = conn.prepareStatement("SELECT * FROM Manufacturer WHERE id=?");
-
+            addManufacturerQuery = conn.prepareStatement("INSERT INTO User VALUES(?,?,?)");
             getManufacturerIDQuery = conn.prepareStatement("SELECT MAX(id)+1 FROM Manufacturer");
             ResultSet rs = getManufacturerIDQuery.executeQuery();
             rs.next();
             freeID = rs.getInt(1);
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-        //refreshListUsers();
     }
 
-//    public Manufacturer getManufacturerFromRS(ResultSet rs){
-//        Manufacturer u = null;
+    public static ManufacturerDAO getInstance() {
+        if (instance == null)
+            instance = new ManufacturerDAO();
+        return instance;
+    }
+
+    public static void removeInstance() {
+        try {
+            instance.conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        instance = null;
+    }
+
+    public Manufacturer getManufacturerFromRS(ResultSet rs){
+        Manufacturer c = null;
 //        try {
-//            Location l = getLocationFromRs()
-//            u = new Manufacturer(rs.getInt(1), rs.getString(2), rs.getInt(3));
+//            City cont = CityDAO.getInstance().getCity(rs.getInt(4));
+//            c = new Manufacturer(rs.getInt(1), rs.getString(2), rs.getInt(3), cont);
 //        } catch (SQLException e) {
 //            //
 //        }
-//        return u;
-//    }
+        return c;
+    }
 
+    public Manufacturer getManufacturer(int id){
+        Manufacturer c = null;
+        try {
+            getManufacturerQueryFromID.setInt(1, id);
+            ResultSet rs = getManufacturerQueryFromID.executeQuery();
+            c = getManufacturerFromRS(rs);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return c;
+    }
+
+    public ObservableList<Manufacturer> getListManufacturer() {
+        return listManufacturers;
+    }
+
+    public Manufacturer addManufacturer(Manufacturer c) {
+//        try {
+//            addManufacturerQuery.setInt(1, freeID);
+//            addManufacturerQuery.setString(2, c.getStreet());
+//            addManufacturerQuery.setInt(3, c.getNumber());
+//            addManufacturerQuery.setInt(4, c.getCity().getId());
+//            addManufacturerQuery.executeUpdate();
+//            c.setId(freeID);
+//            freeID++;
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//
+//        refreshlistManufacturers();
+        return c;
+    }
+
+    private void refreshlistManufacturers(){
+        listManufacturers.clear();
+        listManufacturers.addAll(getAllManufacturers());
+    }
+
+    private ArrayList<Manufacturer> getAllManufacturers() {
+        ArrayList<Manufacturer> result = new ArrayList<>();
+        try {
+            ResultSet rs = allManufacturerQuery.executeQuery();
+            while (rs.next()) {
+                result.add(getManufacturerFromRS(rs));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 }
---
+++ src/ba/unsa/etf/rpr/DAO/NaturalPersonDAO.java
@@ -0,0 +1,111 @@
+package ba.unsa.etf.rpr.DAO;
+
+import ba.unsa.etf.rpr.Location.Location;
+import ba.unsa.etf.rpr.Person.NaturalPerson;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class NaturalPersonDAO {
+    private static NaturalPersonDAO instance = null;
+    private Connection conn;
+    private ObservableList<NaturalPerson> listNaturalPersons = FXCollections.observableArrayList();
+    private PreparedStatement allNaturalPersonQuery, getNaturalPersonQueryFromID, addNaturalPersonQuery, getNaturalPersonIDQuery;
+    private int freeID;
+
+    private NaturalPersonDAO() {
+        conn = MainDAO.getInstance().getConn();
+        try {
+            allNaturalPersonQuery = conn.prepareStatement("SELECT * FROM Natural_person");
+            getNaturalPersonQueryFromID = conn.prepareStatement("SELECT * FROM Natural_person WHERE id=?");
+            addNaturalPersonQuery = conn.prepareStatement("INSERT INTO User VALUES(?,?,?)");
+            getNaturalPersonIDQuery = conn.prepareStatement("SELECT MAX(id)+1 FROM Natural_person");
+            ResultSet rs = getNaturalPersonIDQuery.executeQuery();
+            rs.next();
+            freeID = rs.getInt(1);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static NaturalPersonDAO getInstance() {
+        if (instance == null)
+            instance = new NaturalPersonDAO();
+        return instance;
+    }
+
+    public static void removeInstance() {
+        try {
+            instance.conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        instance = null;
+    }
+
+    public NaturalPerson getNaturalPersonFromRS(ResultSet rs){
+        NaturalPerson c = null;
+        try {
+            Location loc = LocationDAO.getInstance().getLocation(rs.getInt(2));
+            c = new NaturalPerson(rs.getInt(1), loc, rs.getString(3));
+        } catch (SQLException e) {
+            //
+        }
+        return c;
+    }
+
+    public NaturalPerson getNaturalPerson(int id){
+        NaturalPerson c = null;
+        try {
+            getNaturalPersonQueryFromID.setInt(1, id);
+            ResultSet rs = getNaturalPersonQueryFromID.executeQuery();
+            c = getNaturalPersonFromRS(rs);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return c;
+    }
+
+    public ObservableList<NaturalPerson> getListNaturalPerson() {
+        return listNaturalPersons;
+    }
+
+    public NaturalPerson addNaturalPerson(NaturalPerson c) {
+        try {
+            addNaturalPersonQuery.setInt(1, freeID);
+            addNaturalPersonQuery.setInt(2, c.getLocation().getId());
+            addNaturalPersonQuery.setString(3, c.getName());
+            addNaturalPersonQuery.executeUpdate();
+            c.setId(freeID);
+            freeID++;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        refreshlistNaturalPersons();
+        return c;
+    }
+
+    private void refreshlistNaturalPersons(){
+        listNaturalPersons.clear();
+        listNaturalPersons.addAll(getAllNaturalPersons());
+    }
+
+    private ArrayList<NaturalPerson> getAllNaturalPersons() {
+        ArrayList<NaturalPerson> result = new ArrayList<>();
+        try {
+            ResultSet rs = allNaturalPersonQuery.executeQuery();
+            while (rs.next()) {
+                result.add(getNaturalPersonFromRS(rs));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
