d00e2b44d58a088e311494979455fd835441ae13
nhastor1 <nhastor1@etf.unsa.ba>
2020-02-17 23:28:31 +0100

    FrontPageTest

--- test/ba/unsa/etf/rpr/FrontPageTest.java
+++ test/ba/unsa/etf/rpr/FrontPageTest.java
@@ -10,17 +10,25 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.DialogPane;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCombination;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 
 import java.io.File;
 import java.util.ResourceBundle;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(ApplicationExtension.class)
 public class FrontPageTest {
     MainDAO dao;
 
@@ -52,6 +60,12 @@ public class FrontPageTest {
     }
 
     @Test
+    void hasOpened(FxRobot robot) {
+        Button btnLogin = robot.lookup("#btnUser").queryAs(Button.class);
+        assertNotNull(btnLogin);
+    }
+
+    @Test
     void canOpen(FxRobot robot) {
         robot.clickOn("#btnManufacturer").clickOn("#btnBack");
         robot.clickOn("#btnWarehouse").clickOn("#btnBack");
@@ -59,9 +73,30 @@ public class FrontPageTest {
         robot.clickOn("#btnInvoice").clickOn("#btnBack");
         robot.clickOn("#btnProductOrder").clickOn("#btnBack");
         robot.clickOn("#btnProduct").clickOn("#btnBack");
-        robot.clickOn("#btnSearch").clickOn("#btnClose");
-        robot.clickOn("#btnUser").clickOn("#btnFinish");
+        robot.clickOn("#btnSearch").clickOn("#btnCancel");
+        robot.clickOn("#btnUser").clickOn("#btnKraj");
     }
 
+    @Test
+    void changeLanguage(FxRobot robot){
+        robot.clickOn("#menuFile").clickOn("#menuLanguage").clickOn("#menuBosanski");
+        String s = robot.lookup("#lblMainMenu").queryAs(Label.class).getText();
+        assertEquals("Glavni meni", s);
+
+        robot.clickOn("#menuFile").clickOn("#menuLanguage").type(KeyCode.DOWN).type(KeyCode.ENTER);
+        s = robot.lookup("#lblMainMenu").queryAs(Label.class).getText();
+        assertEquals("Main menu", s);
+    }
+
+    @Test
+    void print(FxRobot robot){
+        robot.clickOn("#menuFile").clickOn("#menuSave").sleep(200).write("test").clickOn();
+        robot.lookup(".dialog-pane").tryQuery().isPresent();
+        DialogPane dialogPane = robot.lookup(".dialog-pane").queryAs(DialogPane.class);
+        Button okButton = (Button) dialogPane.lookupButton(ButtonType.OK);
+        robot.clickOn(okButton);
+        File f = new File("test");
+        assertTrue(f.exists());
+    }
 
 }
